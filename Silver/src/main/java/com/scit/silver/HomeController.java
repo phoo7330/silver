d94d68a9051176b33c0e92b5beb58ac192fdfc1e
Name명 <@naver.com>
2019-03-11 21:09:00 +0900

    login

--- Silver/src/main/java/com/scit/silver/HomeController.java
+++ Silver/src/main/java/com/scit/silver/HomeController.java
@@ -4,6 +4,8 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +14,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.scit.silver.vo.Member;
+import com.scit.silver.dao.MemberDAO;
 import com.scit.silver.dao.testdao;
 import com.scit.silver.vo.test;
 
@@ -23,6 +27,7 @@ public class HomeController {
 	
 	@Autowired
 	testdao dao;
+	
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
@@ -50,4 +55,5 @@ public class HomeController {
 		
 		return "redirect:/";
 	}
-}
+	
+	}
--- Silver/src/main/webapp/WEB-INF/views/home.jsp
+++ Silver/src/main/webapp/WEB-INF/views/home.jsp
@@ -10,10 +10,25 @@
 	실버서퍼 시작 1
 </h1>
 
+<c:if test="${sessionScope.loginId!=null}">
+<h1>${sessionScope.loginId}님 환영합니다.</h1>
+
+	<h3><a href="logout">로그아웃</a></h3>
+</c:if>
+
+<c:if test="${sessionScope.loginId==null}">
+		<h3><a href="signup">회원가입하기</a></h3>
+		<h3><a href="login">로그인하기</a></h3>
+	</c:if>
+
+
+
 <form action="insertTest">
 id:<input type="text" name="id">
 pw:<input type="password" name="pw">
 <input type="submit" value="전송">
 </form>
+
+
 </body>
 </html>
