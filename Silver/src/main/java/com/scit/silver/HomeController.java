1c49ed0df839cbac47ea0c224a638ac894a6d638
user <user@DESKTOP-32LS54K>
2019-03-19 18:04:38 +0900

    test 삭제.

--- Silver/src/main/java/com/scit/silver/HomeController.java
+++ Silver/src/main/java/com/scit/silver/HomeController.java
@@ -1,9 +1,6 @@
 package com.scit.silver;
 
-import java.util.Locale;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -14,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController {
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
+	public String index() {
 		
 		return "index";
 	}
