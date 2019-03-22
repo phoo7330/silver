5c56aebf364594c07a44ab6bb9a49af0c7addea4
jongwonlim <olg9990@naver.com>
2019-03-22 11:17:57 +0900

    2019-03-22 limjongwon member테이블 컬럼 추가로인해 vo(member) , memberMapper.xml 수정

--- Silver/src/main/java/com/scit/silver/vo/Member.java
+++ Silver/src/main/java/com/scit/silver/vo/Member.java
@@ -13,9 +13,9 @@ public class Member {
 	private	String registration;
 	private	String email;
 	private	String warning;
-	
+	private String employee_number;
 	public Member(int code_num, int type, String userid, String userpwd, String username, String birthday,
-			String gender, String address, String registration, String email, String warning) {
+			String gender, String address, String registration, String email, String warning, String employee_number) {
 		super();
 		this.code_num = code_num;
 		this.type = type;
@@ -28,107 +28,88 @@ public class Member {
 		this.registration = registration;
 		this.email = email;
 		this.warning = warning;
+		this.employee_number = employee_number;
 	}
-
 	public Member() {
 		super();
 	}
-
 	public int getCode_num() {
 		return code_num;
 	}
-
 	public void setCode_num(int code_num) {
 		this.code_num = code_num;
 	}
-
 	public int getType() {
 		return type;
 	}
-
 	public void setType(int type) {
 		this.type = type;
 	}
-
 	public String getUserid() {
 		return userid;
 	}
-
 	public void setUserid(String userid) {
 		this.userid = userid;
 	}
-
 	public String getUserpwd() {
 		return userpwd;
 	}
-
 	public void setUserpwd(String userpwd) {
 		this.userpwd = userpwd;
 	}
-
 	public String getUsername() {
 		return username;
 	}
-
 	public void setUsername(String username) {
 		this.username = username;
 	}
-
 	public String getBirthday() {
 		return birthday;
 	}
-
 	public void setBirthday(String birthday) {
 		this.birthday = birthday;
 	}
-
 	public String getGender() {
 		return gender;
 	}
-
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
-
 	public String getAddress() {
 		return address;
 	}
-
 	public void setAddress(String address) {
 		this.address = address;
 	}
-
 	public String getRegistration() {
 		return registration;
 	}
-
 	public void setRegistration(String registration) {
 		this.registration = registration;
 	}
-
 	public String getEmail() {
 		return email;
 	}
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
 	public String getWarning() {
 		return warning;
 	}
-
 	public void setWarning(String warning) {
 		this.warning = warning;
 	}
-
+	public String getEmployee_number() {
+		return employee_number;
+	}
+	public void setEmployee_number(String employee_number) {
+		this.employee_number = employee_number;
+	}
 	@Override
 	public String toString() {
 		return "Member [code_num=" + code_num + ", type=" + type + ", userid=" + userid + ", userpwd=" + userpwd
 				+ ", username=" + username + ", birthday=" + birthday + ", gender=" + gender + ", address=" + address
-				+ ", registration=" + registration + ", email=" + email + ", warning=" + warning + "]";
+				+ ", registration=" + registration + ", email=" + email + ", warning=" + warning + ", employee_number="
+				+ employee_number + "]";
 	}
-	
-	
-	
 }
