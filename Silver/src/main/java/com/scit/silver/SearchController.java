d1e052bb4038a9f9d3cdc0e14424fd904bbcbc2a
Danei <951753pm@naver.com>
2019-03-26 18:05:30 +0900

    Search test

--- Silver/src/main/java/com/scit/silver/SearchController.java
+++ Silver/src/main/java/com/scit/silver/SearchController.java
@@ -92,9 +92,7 @@ public class SearchController {
 	public String searchDetail(int seach_seq) {
 
 		System.out.println(seach_seq);
-		if(seach_seq==0) {
-			return "searchDetail2";
-		}
+		
 		return "redirect:/search";
 	}
 }
